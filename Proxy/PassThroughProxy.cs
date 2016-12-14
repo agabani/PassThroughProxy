0b4e4b35b6179c2bcb3a5a30784594e0bfce42e2
Ahmed Agabani <agabani@live.com>
2016-12-14 23:57:38 +0000

    Changed dispose signature

--- Proxy/PassThroughProxy.cs
+++ Proxy/PassThroughProxy.cs
@@ -21,7 +21,7 @@ namespace Proxy
             GC.SuppressFinalize(this);
         }
 
-        private void Dispose(bool disposing)
+        protected virtual void Dispose(bool disposing)
         {
             if (disposing)
             {
