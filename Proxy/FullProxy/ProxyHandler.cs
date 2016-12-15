fef9b4d35390491197c1edac8619dd21712f24c3
Ahmed Agabani <agabani@live.com>
2016-12-15 18:03:37 +0000

    Extracted tunnel proxy handler

--- Proxy/FullProxy/ProxyHandler.cs
+++ Proxy/FullProxy/ProxyHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using System.Net.Sockets;
-using System.Text;
+﻿using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,7 +11,7 @@ namespace Proxy.FullProxy
             using (client)
             using (var clientStream = client.GetStream())
             {
-                var httpHeader = await new HttpsHost.HttpHeaderStream()
+                var httpHeader = await new HttpHeaderStream()
                     .GetHeader(clientStream, CancellationToken.None);
 
                 if (httpHeader == null)
@@ -23,34 +21,9 @@ namespace Proxy.FullProxy
 
                 if (httpHeader.Verb == "CONNECT")
                 {
-                    await TunnelConnection(httpHeader, clientStream);
+                    await new ProxyTunnelHandler().Run(httpHeader, clientStream);
                 }
             }
-        }
-
-        private static async Task TunnelConnection(HttpsHost.HttpHeader httpHeader, NetworkStream clientStream)
-        {
-            using (var host = await CreateHost(httpHeader))
-            using (var hostStream = host.GetStream())
-            using (var tunnel = new TcpTunnel(clientStream, hostStream))
-            {
-                var task = tunnel.Run();
-                await SendConnectionEstablised(clientStream);
-                await task;
-            }
-        }
-
-        private static async Task<TcpClient> CreateHost(HttpsHost.HttpHeader httpHeader)
-        {
-            var host = new TcpClient();
-            await host.ConnectAsync(httpHeader.Host.Hostname, httpHeader.Host.Port);
-            return host;
-        }
-
-        private static async Task SendConnectionEstablised(Stream stream)
-        {
-            var bytes = Encoding.ASCII.GetBytes("HTTP/1.1 200 Connection established\r\n\r\n");
-            await stream.WriteAsync(bytes, 0, bytes.Length);
         }
     }
 }
